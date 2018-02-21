c4c0b58208273e3fa69cf49d322b68b063167469
owenliversidge <owen.liversidge@gmail.com>
2018-02-21 16:30:06 -0500

    updated readme

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'hbs')
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
+// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
 app.use(logger('dev'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -59,7 +59,7 @@ app.use(function(err, req, res, next) {
   res.render('error')
 })
 
-//APP LISTENING set app listening on 4000 for postman application
+// app listening on 4000 for postman application
 app.listen(4000, () => {
   console.log('Meat is sizzling on port 4000')
 })
