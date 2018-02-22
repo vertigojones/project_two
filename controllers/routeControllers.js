0a437496f90ce651a000cc7e5c36cc4679c11cb3
owenliversidge <owen.liversidge@gmail.com>
2018-02-22 12:25:58 -0500

    added create routes

--- controllers/routeControllers.js
+++ controllers/routeControllers.js
@@ -3,10 +3,9 @@ const router = express.Router()
 const MeatUser = require('../models/user')
 
 // INDEX
-// GET
 router.get('/', (req, res) => {
     // get all meat users
-    MeatUser.find().then((attendees => {
+    MeatUser.find().then((attendee) => {
         // send all meat users to homepage
         res.render('../views/index', {
             attendee: attendee
@@ -16,5 +15,27 @@ router.get('/', (req, res) => {
 
 // NEW 
 router.get('/new', (req, res) => {
-
+    res.render('user/new')
 })
+
+// CREATE
+router.post('/', (req, res) => {
+    const newMeatUser= new MeatUser({
+      name: req.body.name,
+      gender: req.body.gender,
+      age: req.body.age,
+      hobbies: req.body.hobbies,
+      meat: req.body.meat,
+      preparation: req.body.preparation,
+      side: req.body.side
+    })
+  
+    // save the new company
+    newMeatUser.save().then((savedMeatUser) => {
+  
+      // redirect to the new companies page
+      res.redirect(`/user/${savedMeatUser._id}`)
+    })
+  })
+
+
