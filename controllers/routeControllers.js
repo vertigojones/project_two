4a4e65b4991dad5fb1afab44ac15627ae87c18f7
owenliversidge <owen.liversidge@gmail.com>
2018-02-22 12:11:01 -0500

    added basic homepage

--- controllers/routeControllers.js
+++ controllers/routeControllers.js
@@ -1,9 +1,20 @@
 const express = require('express')
 const router = express.Router()
-const Company = require('../models/user')
+const MeatUser = require('../models/user')
 
+// INDEX
 // GET
 router.get('/', (req, res) => {
-    // test connection
-    res.send('welcome to my page')
+    // get all meat users
+    MeatUser.find().then((attendees => {
+        // send all meat users to homepage
+        res.render('../views/index', {
+            attendee: attendee
+        })
+    })
+})
+
+// NEW 
+router.get('/new', (req, res) => {
+
 })
